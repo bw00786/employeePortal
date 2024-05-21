7694df34f1253e9d4a35becdb2d48e4f9dd48b03
Wilkins, Bruce <Bruce.Wilkins@sscinc.com>
2024-05-21 16:54:36 -0400

    updated more variables

--- src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
+++ src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
@@ -18,6 +18,7 @@ public class EmployeeEvent {
     private String num_nits;
     private String num_nits2;
     private Integer fake_rues_dasdas_dasdsadas_dasdsadas;
+    private String ringWork;
 }
 
 
