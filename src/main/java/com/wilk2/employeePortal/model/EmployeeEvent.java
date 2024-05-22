9c4d0b7045a33ef2e8cabaf9dfe0773e0fcbad0d
Wilkins, Bruce <Bruce.Wilkins@sscinc.com>
2024-05-22 07:23:47 -0400

    another method added

--- src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
+++ src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
@@ -15,6 +15,11 @@ public class EmployeeEvent {
     private String employeeID;
     private String employeeStartDate;
    private String watch_men;
+   private String watcvh_dasdas_dadasda;
+
+   String sdfsdfsdfsdfsd() {
+       return null;
+   }
 }
 
 
