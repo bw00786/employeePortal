065ff35bc1e1e04fb6cc5e9f9537d86489ac5f20
Wilkins, Bruce <Bruce.Wilkins@sscinc.com>
2024-05-23 13:13:48 -0400

    remove main and update

--- src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
+++ src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
@@ -14,6 +14,12 @@ public class EmployeeEvent {
     private String email;
     private String employeeID;
     private String employeeStartDate;
+    private Integer dadsa_dasdasdsadasda_dadas;
+    private String dadasdasdasdads_dasdas_dadasd;
+
+    private void dadasdasd(String dadasdasdasdads_dasdas_dadasd) {
+
+    }
 
 
 }
