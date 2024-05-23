5d4b062cfa1177ac109c4cd7b394c8d7c1341d1d
Wilkins, Bruce <Bruce.Wilkins@sscinc.com>
2024-05-23 13:38:30 -0400

    removed bad code

--- src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
+++ src/main/java/com/wilk2/employeePortal/model/EmployeeEvent.java
@@ -14,12 +14,9 @@ public class EmployeeEvent {
     private String email;
     private String employeeID;
     private String employeeStartDate;
-    private Integer dadsa_dasdasdsadasda_dadas;
-    private String dadasdasdasdads_dasdas_dadasd;
 
-    private void dadasdasd(String dadasdasdasdads_dasdas_dadasd) {
 
-    }
+
 
 
 }
